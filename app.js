f535018b1117a29c5828a8d614101a7562174517
uuree <9911gold@gmail.com>
2020-10-25 07:09:59 +0800

    SHoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ var scores=[0,0];
 
 var roundScore=0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice=Math.floor(Math.random()*6)+1;
+var diceNumber=Math.floor(Math.random()*6)+1;
 
-// <div class="player-score" id="score-0">43</div>
 // Програм эхлэхэд бэлтгэе
-window.document.querySelector('#score-0').textContent=0;
-// document.querySelector('#score-1').innerHTML='<em>0</em>';
-window.document.querySelector('#score-1').textContent=0;
-document.querySelector('.dice').style.display='none';
-document.querySelector('#current-0').textContent=0;
-document.querySelector('#current-1').textContent=0;
 
-console.log('Шоо :'+dice);
+
+document.getElementById('score-0').textContent=0;
+document.getElementById('score-1').textContent=0;
+document.getElementById('current-0').textContent=0;
+document.getElementById('current-1').textContent=0;
+
+var diceDom=document.querySelector('.dice');
+
+document.querySelector('.btn-roll').addEventListener("click", function (){
+    var diceNumber=Math.floor(Math.random()*6)+1;
+    diceDom.style.display="blok";
+    diceDom.src='dice-'+diceNumber+'.png';
+   
+})
